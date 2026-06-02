e8b99e98a587c1a7613dc95a15f0ef630949abf6
Janine Rae <105533574+janrae-l@users.noreply.github.com>
2026-06-02 14:46:21 +0800

    Add addTodo method to the module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 "use strict";
 
 class TodoList {
-  constructor() {
+  constructor(title, dueDate, description) {
     this.title = title;
     this.dueDate = dueDate;
     this.description = description;
     this.id = crypto.randomUUID();
   }
 }
+
+const addTodo = {
+  add() {
+    const todo = new TodoList(title, dueDate, desicription);
+    arr.push(todo);
+    return todo;
+  },
+};
