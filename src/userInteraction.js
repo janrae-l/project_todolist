6f31f79a2e64b95ad2bb5b1cdc0460cd1396b258
Janine Rae <105533574+janrae-l@users.noreply.github.com>
2026-06-23 20:59:28 +0800

    Add DOM components to the todo item button

--- src/userInteraction.js
+++ src/userInteraction.js
@@ -1,4 +1,8 @@
+//target the body container
 const bigContainer = document.querySelector("body");
 
 //Project confirm button
 const newProj = document.querySelector(".create-proj");
+
+//Todo item confirm button
+const todoItemBtn = document.querySelector(".todoBtn");
