c647ff5a048ed9058b8e2831fcbc1c6a590d916d
Janine Rae <105533574+janrae-l@users.noreply.github.com>
2026-06-24 16:58:35 +0800

    Add event listener with if else statements for each important buttons and its logic in every button

--- src/userInteraction.js
+++ src/userInteraction.js
@@ -1,8 +1,31 @@
 //target the body container
 const bigContainer = document.querySelector("body");
 
-//Project confirm button
-const newProj = document.querySelector(".create-proj");
+//Create Project button
+const createProj = document.querySelector(".create-proj");
+createProj.setAttribute("id", "createProject")
+
+//Confirm Project Button
+const confirmProjBtn = document.querySelector("confirmProj")
+confirmProjBtn.setAttribute("id", "confirmProj")
+
+//Add todo item
+const addTodoBtn = document.querySelector(".add-todo")
+addTodoBtn.setAttribute("id", "addTodo")
 
 //Todo item confirm button
 const todoItemBtn = document.querySelector(".todoBtn");
+todoItemBtn.setAttribute("id", "todoItem")
+
+const projDialog = document.querySelector(".proj-dialog");
+const todoDialog = document.querySelector(".todo-dialog");
+
+bigContainer.addEventListener("click", (event) => {
+    if (event.target.id === "createProject") {
+        console.log(event.target.textContent);
+        projDialog.showModal();
+    } else if (event.target.id === "confirmProj") {
+        //gets the details filled in by the user
+        //the details are applied in the Project Class
+    } else if (event.target.id === "")
+});
