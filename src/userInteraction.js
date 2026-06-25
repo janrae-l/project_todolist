f13622119e91cc802ad5538507a975d15cbc2e94
Janine Rae <105533574+janrae-l@users.noreply.github.com>
2026-06-25 23:26:46 +0800

    Add wrapper function to get the user input

--- src/userInteraction.js
+++ src/userInteraction.js
@@ -3,29 +3,40 @@ const bigContainer = document.querySelector("body");
 
 //Create Project button
 const createProj = document.querySelector(".create-proj");
-createProj.setAttribute("id", "createProject")
+createProj.setAttribute("id", "createProject");
 
 //Confirm Project Button
-const confirmProjBtn = document.querySelector("confirmProj")
-confirmProjBtn.setAttribute("id", "confirmProj")
+const confirmProjBtn = document.querySelector("confirmProj");
+confirmProjBtn.setAttribute("id", "confirmProj");
 
 //Add todo item
-const addTodoBtn = document.querySelector(".add-todo")
-addTodoBtn.setAttribute("id", "addTodo")
+const addTodoBtn = document.querySelector(".add-todo");
+addTodoBtn.setAttribute("id", "addTodo");
 
 //Todo item confirm button
-const todoItemBtn = document.querySelector(".todoBtn");
-todoItemBtn.setAttribute("id", "todoItem")
+const confirmTodo = document.querySelector(".confirmTodo");
+todoItemBtn.setAttribute("id", "confirmTodo");
 
 const projDialog = document.querySelector(".proj-dialog");
 const todoDialog = document.querySelector(".todo-dialog");
 
+function projectWrapper() {
+  const titleValue = document.querySelector("#projectTitle").value;
+  //how to send the returned value to another module since it is needed in the core logic
+}
+
 bigContainer.addEventListener("click", (event) => {
-    if (event.target.id === "createProject") {
-        console.log(event.target.textContent);
-        projDialog.showModal();
-    } else if (event.target.id === "confirmProj") {
-        //gets the details filled in by the user
-        //the details are applied in the Project Class
-    } else if (event.target.id === "")
+  if (event.target.id === "createProject") {
+    console.log(event.target.textContent);
+    projDialog.showModal();
+  } else if (event.target.id === "confirmProj") {
+    //gets the details filled in by the user
+    //the details are applied in the Project Class
+  } else if (event.target.id === "addTodo") {
+    console.log(event.target.textContent);
+    todoDialog.showModal();
+  } else if (event.target.id === "confirmTodo") {
+    //gets the user input details and populates the Todo Item Class using the user input details
+    //this needs to happen not in the same user interface
+  }
 });
